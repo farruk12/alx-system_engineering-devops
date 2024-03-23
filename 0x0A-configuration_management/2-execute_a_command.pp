aac2b112df2957ad54a05658a8db710af29f16fe
farruk12 <farruk.omar@icloud.com>
2024-03-23 01:18:21 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,4 @@
-# create a Puppet manifest that kills a process named "killmenow" using the exec resource and pkill
-
-exec { 'kill_killmenow_process':
-  command     => '/usr/bin/pkill killmenow',
-  path        => '/usr/bin:/bin',
-  refreshonly => true,
-  onlyif      => '/usr/bin/pgrep killmenow',
+# Executes a command
+exec { 'pkill killmenow':
+  path => '/usr/bin:/usr/sbin:/bin'
 }
